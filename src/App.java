afc1d3ad964e1ed4452ecdd48be782fd54fa7def
Jose Velaides <creativelaides@gmail.com>
2023-09-18 15:01:46 -0500

    feat: :sparkles: Ejercicio de Evidencia 2
    Ejercicio de Evidencia 2 Finalizado con introduccion de nuevos metos y optimización de los mismos usando expresiones lampdas.
    
    Se cambia Console por Scanner como metodo de entrada de datos

--- src/App.java
+++ src/App.java
@@ -1,101 +1,111 @@
-import java.io.Console;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-
-        //  Menu 1
-        System.out.println(
-            "Hola Bienvenido al E-Commerce de la Tienda X \n" +
-            "¿Qué deseas hacer? \n" +
-            "1. Revisar las ventas \n" +
-            "2. Ingresar una venta \n"
-            );
-
-        int option = Integer.parseInt(System.console().readLine());
-        
-        while (option != 1 || option != 2) {
-            
-            System.out.println(
-                "Numero erróneo \n" +
-                "Ingresa una opción correcta \n"
-                );
-
-            option = Integer.parseInt(System.console().readLine());
-        };
-
-        // Menu 2
-        System.out.println(
-            "Por favor selecciona el día de la semana \n" +
-            "1. Lunes \n" +
-            "2. Martes \n" +
-            "3. Miércoles \n" +
-            "4. Jueves \n" +
-            "5. Viernes \n" +
-            "6. Sábado \n" +
-            "7. Domingo \n"
-            );
-
-        int day = Integer.parseInt(System.console().readLine());
-
-        // Venta
-        do {
-            System.out.println(
-                "Por favor ingresa el monto de la venta \n"
-                );
-
-            double sale = Double.parseDouble(System.console().readLine());
-
-            System.out.println(
-                "¡Venta registrada con éxito!\n\n" +
-                "¿Deseas ingresar otra venta? \n" +
-                "1. Si \n" +
-                "2. No \n");
-
-            option = Integer.parseInt(System.console().readLine());
-            do {
-                System.out.println(
-                    "Numero erróneo \n" +
-                    "Ingresa una opción correcta \n"
-                    );
-            } while (option != 1 || option != 2);
-            
-        } while (option == 1);
+        Scanner scanner = new Scanner(System.in);
 
         // Crear un conjunto de claves ÚNICAS
         final List<String> daysKeys = new ArrayList<>(List.of(
-            "Lunes", 
-            "Martes", 
+            "Lunes",
+            "Martes",
             "Miércoles",
             "Jueves",
             "Viernes",
             "Sábado",
             "Domingo"
-        ));
+            )
+        );
 
-        // Crear un diccionario con claves de tipo String y valores de tipo List<Double>
-        HashMap<String, List<Double>> ecommerce = new HashMap<>();
+        // Diccionario para almacenar las ventas de la semana
+        HashMap<String, List<Double>> salesWeek = new HashMap<>();
 
-        // Agregar elementos al diccionario
-        
+        int menuChoice;
+        do {
+            // Menú principal
+            System.out.println(
+                "Hola Bienvenido al E-Commerce de la Tienda X \n" +
+                "¿Qué deseas hacer? \n" +
+                "1. Revisar las ventas \n" +
+                "2. Ingresar una venta \n" +
+                "3. Mostrar días con ventas superiores a $700,000 \n" +
+                "4. Salir \n"
+            );
 
-       /*     List<Integer> valores2 = new ArrayList<>();
-        valores2.add(3);
-        valores2.add(4);
-        valores2.add(5);
-        dictionary.put("clave2", valores2);
+            menuChoice = scanner.nextInt();
+            scanner.nextLine(); // Consumir la nueva línea después de leer un entero
 
-        // Acceder a elementos en el diccionario
-        List<Integer> valoresDeClave1 = dictionary.get("clave1");
-        System.out.println("Valores asociados con clave1: " + valoresDeClave1);
+            switch (menuChoice) {
+                case 1:
+                    // Consultar ventas de la semana
+                    for (Map.Entry<String, List<Double>> entry : salesWeek.entrySet()) {
+                        String weekDay = entry.getKey();
+                        List<Double> saleDay = entry.getValue();
+                        System.out.println("Día: " + weekDay + ", Ventas: " + saleDay);
+                    }
+                    break;
+                case 2:
+                    // Ingresar una venta
+                    // Seleccionar el día de la semana
+                    System.out.println(
+                        "Por favor selecciona el día de la semana \n" +
+                        "1. Lunes \n" +
+                        "2. Martes \n" +
+                        "3. Miércoles \n" +
+                        "4. Jueves \n" +
+                        "5. Viernes \n" +
+                        "6. Sábado \n" +
+                        "7. Domingo \n"
+                    );
 
-        // Iterar con un for-each a través del diccionario
-        for (Map.Entry<String, List<Integer>> entry : dictionary.entrySet()) {
-            String clave = entry.getKey();
-            List<Integer> valores = entry.getValue();
-            System.out.println("Clave: " + clave + ", Valores: " + valores);
-        } */
+                    int day;
+                    do {
+                        day = scanner.nextInt() - 1;
+                        scanner.nextLine(); // Consumir la nueva línea después de leer un entero
+
+                        if (day >= 0 && day < daysKeys.size()) {
+                            System.out.println("Seleccionaste el día: " + daysKeys.get(day));
+                        } else {
+                            System.out.println("Ingresa una opción válida");
+                        }
+                    } while (day < 0 || day >= daysKeys.size());
+
+                    // Ingresar el monto de la venta
+                    System.out.println("Por favor ingresa el monto de la venta:");
+                    double sale = scanner.nextDouble();
+                    scanner.nextLine(); // Consumir la nueva línea después de leer un double
+
+                    // Agregar la venta a la lista correspondiente
+                    String selectedDay = daysKeys.get(day);
+                    salesWeek.computeIfAbsent(selectedDay, k -> new ArrayList<>()).add(sale);
+
+                    System.out.println("Venta registrada en el día " + selectedDay);
+                    break;
+                case 3:
+                    // Mostrar días con ventas superiores a $700,000
+                    System.out.println("Días con ventas superiores a $700,000:");
+                    for (Map.Entry<String, List<Double>> entry : salesWeek.entrySet()) {
+                        String weekDay = entry.getKey();
+                        List<Double> saleDay = entry.getValue();
+                        double totalSales = saleDay.stream().mapToDouble(Double::doubleValue).sum();
+                        if (totalSales > 700000) {
+                            System.out.println("Día: " + weekDay + ", Ventas Totales: $" + totalSales);
+                        }
+                    }
+                    break;
+                case 4:
+                    System.out.println("Gracias por usar el programa. ¡Hasta luego!");
+                    break;
+                default:
+                    System.out.println("Ingresa una opción válida");
+                    break;
+            }
+
+        } while (menuChoice != 4);
+
+        scanner.close();
     }
 }
